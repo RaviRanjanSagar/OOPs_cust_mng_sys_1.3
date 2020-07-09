7d04e92dfd4ce25e7c3dcb0745d40b526c68a6a1
Ravi Sagar <ravirsagar08@gmail.com>
2020-07-09 21:38:40 +0530

    CustomerMS 1.1.A

---
+++ FileHandelings/filehandeling_1.py
@@ -0,0 +1,35 @@
+#  FILE HANDELINGs
+
+# #DAY 1st
+
+# f = open(r"R:\FILE HANDELING_files\Rav.txt"","r")
+# data = f.read()
+# print(data)
+# print(len(data))
+# print(type(data))
+#f.close()
+
+# f = open(r"R:\FILE HANDELING_files\Rav.txt","r")
+# s = f.readline()
+# print(s)                    #<--GAP---->
+# s = f.readline()
+# print(s)
+#f.close()
+
+# file = open(r"R:\FILE HANDELING_files\Rav.txt","r")
+# data = file.readlines()
+# print(data)     #LIST DATA TYPE
+# print(type(data))
+# print(len(data))
+# file.close()                # COMPULSARY STEP
+
+
+#               WRITE OPERATIONS
+
+"New file gets created"
+# file = open(r"R:\FILE HANDELING_files\Rav_1.txt","w")       #NO file exists there ... so new one gets automatically created ..
+# file.close()
+
+
+
+
